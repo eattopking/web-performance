1a4398793e2dfe4751a0735c9035737395db7cf5
zhangheng05 <zhangheng05@baijiahulian.com>
2021-05-10 20:28:21 +0800

    feat: 更新

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+bbb='444'
+ccc='333'
+vvvv=(6 6 7 8 9)
+if [ $bbb == $ccc ]
+then
+  echo 1111
+elif [ $ccc == '333' ]
+then
+  echo 222
+fi
+
+for vv in ${vvvv[@]}
+    do
+     echo ${vvvv[@]}
+
+    done
+
+dffff() {
+    echo $1 $2 ${11}
+}
+
+dffff 1 1 3 4 5 6 6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+
+
