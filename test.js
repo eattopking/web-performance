e732f0603a37186651083fc52e07c1871b42067c
zhanghengh <zhanghengh@yonyou.com>
2021-08-19 15:44:04 +0800

    feat: 更新

--- test.js
+++ test.js
@@ -2,4 +2,63 @@
 
 const process = require('process');
 
-console.log('1111111', JSON.parse(JSON.stringify(process.env)))
+function deepClone(obj) {
+    let result = null;
+    if (typeof obj !== 'object') {
+      if (typeof obj !== 'function') {
+        return result;
+      } else {
+        return obj;
+      }
+    }
+
+    if (obj === null) {
+      return result;
+    }
+
+    if (obj instanceof Array) {
+      result = [];
+      for(let i = 0; i < obj.length; i++) {
+        const item = obj[i];
+        result[i] = deepClone(item);
+      }
+    } else if (obj instanceof Set){
+      result = new Set();
+      obj.forEach((val) => {
+        result.add(deepClone(val));
+      });
+    } else if (obj instanceof Map){
+      result = new Map();
+      obj.forEach((val, key) => {
+        result.set(key, deepClone(val));
+      });
+    } else if (Object.prototype.toString.call(obj) === "[object Object]") {
+      result = {};
+      Objeck.keys(obj).forEach((key) => {
+        result[key] = deepClone(obj[obj]);
+      });
+    }
+    return result;
+}
+
+let a = [1,2,3];
+let b = [];
+
+function add(val) {
+  a.push(val);
+}
+
+function get() {
+  let alen = a.len;
+  while(alen) {
+    alen--;
+    b.push(a.pop());
+  }
+  const reuslt = b.pop();
+  const blen = b.length;
+  while(blen) {
+    blen--;
+    a.push(b.pop());
+  }
+  return reuslt;
+}
