b0390a78f66423d28bf6ae05e68794bfa9b87448
zhanghengh <zhanghengh@yonyou.com>
2022-12-02 15:31:10 +0800

    feat: 优化目录

--- demo/node/index.js
+++ demo/node/index.js
@@ -87,3 +87,44 @@ const writeStream = fs.createWriteStream('/Users/zhangheng/lx/web-performance/cc
 const readStream = fs.createReadStream('/Users/zhangheng/lx/web-performance/ccc/sss.js');
 
 readStream.pipe(writeStream);
+
+
+/ var child = require('child_process');
+
+// const scholarProcess = child.fork('/Users/zhangheng/lx/web-performance/test1.js', {
+  
+//   detached: false,
+//   silent: true,
+// });
+// let message = '';
+// scholarProcess.stdin.on('data', (data) => {
+//   console.log('data1111', data.toString())
+//   message += data.toString() + '\n';
+// });
+// scholarProcess.stderr.on('data', (data) => {
+//   console.log('data222', data.toString())
+//   message = '!!!' + data.toString() + '\n';
+// });
+// scholarProcess.on('error', (data) => {
+//   console.log('data333', data)
+//   message = '!!!' + data.toString() + '\n';
+// });
+// scholarProcess.on('exit', () => {
+//   console.log('scholarProcess =====');
+//   console.log('4444', message);
+//   console.log('scholarProcess =====');
+// });
+
+// console.log('进程id2',process.pid)
+
+// const { execFile } = require('child_process');
+// const child = execFile('node',['--version'], {shell: true}, (error, stdout, stderr) => {
+//   if (error) {
+//     throw error;
+//   }
+//   console.log(stdout);
+// });
+
+// child.stdout.on('data', (data) => {
+//   console.log(555555, data);
+// })
