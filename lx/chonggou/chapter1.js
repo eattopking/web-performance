e71b7e75e48bf0aec94fa2ac1402fbb4fe81deed
zhanghengh <zhanghengh@yonyou.com>
2024-11-03 21:17:38 +0800

    feat: 更新重构练习

--- lx/chonggou/chapter1.js
+++ lx/chonggou/chapter1.js
@@ -107,45 +107,6 @@ function newStatement3 (invoice, plays) {
     return result;
 }
 
-function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
-}
-
-function playFor(aPerformance) {
-    return plays[aPerformance.playID]
-}
-
-const amountFor = (aPerformance) => {
-    let result = 0;
-    switch (playFor(aPerformance).type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) { 
-                result += 1000 * (aPerformance.audience - 30); 
-            }
-        break;
-        case "comedy":
-            result = 30000;
-            if (aPerformance.audience > 20) { 
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience; 
-        break;
-        default:
-        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-    }
-    return result;
-}
-
-function volumeCreditsFor(perf) {
-    let result = 0;
-    // add volume credits
-    result += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === playFor(perf).type) result += Math.floor(perf.audience / 5);
-    return result;
-}
-
 function newStatement4 (invoice) {
     let totalAmount = 0;
     let volumeCredits = 0;
@@ -161,4 +122,81 @@ function newStatement4 (invoice) {
     return result;
 }
 
-console.log(newStatement4(invoices[0]))
+function renderPlainText (data, plays) {
+    let result = `Statement for ${data.customer}\n`; 
+    for (let aPerformance of data.performances) {
+        // print line for this order
+        result += ` ${playFor(aPerformance).name}: ${usd(amountFor(aPerformance))} (${aPerformance.audience} seats)\n`; 
+    }
+
+
+    result += `Amount owed is ${usd(totalAmount())}\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`; 
+    return result;
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
+    }
+    
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID]
+    }
+    
+    function amountFor (aPerformance) {
+        let result = 0;
+        switch (playFor(aPerformance).type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) { 
+                    result += 1000 * (aPerformance.audience - 30); 
+                }
+            break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) { 
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience; 
+            break;
+            default:
+            throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+        }
+        return result;
+    }
+    
+    function volumeCreditsFor(perf) {
+        let result = 0;
+        // add volume credits
+        result += Math.max(perf.audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+        if ("comedy" === playFor(perf).type) result += Math.floor(perf.audience / 5);
+        return result;
+    }
+    
+    function totalVolumeCredits() {
+        let result = 0;
+        for (let aPerformance of data.performances) {
+            // add volume credits
+            result += volumeCreditsFor(aPerformance);
+        }
+    
+        return result;
+    }
+
+    function totalAmount() {
+        let result = 0;
+        for (let aPerformance of data.performances) {
+            result += amountFor(aPerformance);
+        }
+        return result;
+    }
+}
+
+function newStatement5(invoice, plays) {
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances;
+    return renderPlainText(statementData, plays);
+}
+
+console.log(newStatement5(invoices[0], plays))
